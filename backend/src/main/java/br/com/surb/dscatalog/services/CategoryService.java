78c97c64c7d30f2e080d4a7fe12b41a0445c3bd8
claudiobelempb <claudio.c.lima@hotmail.com>
2021-09-13 20:13:28 -0300

    Banco de dados SEEDING

--- backend/src/main/java/br/com/surb/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/surb/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import br.com.surb.dscatalog.entities.Category;
 import br.com.surb.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
   @Autowired
   private CategoryRepository repository;
 
+  @Transactional(readOnly = true)
   public List<Category> findAll() {
     return repository.findAll();
   }
