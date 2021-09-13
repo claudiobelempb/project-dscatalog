173290436d5a0e051c4050de559acbf5e1462482
claudiobelempb <claudio.c.lima@hotmail.com>
2021-09-13 14:42:08 -0300

    start project

---
+++ backend/src/main/java/br/com/surb/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
