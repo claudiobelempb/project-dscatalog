c9ef9f44f2c01a5ae6ec41160fdab8e0bac3d332
claudiobelempb <claudio.c.lima@hotmail.com>
2021-09-16 13:41:03 -0300

    Entity Product

---
+++ backend/src/main/java/br/com/surb/dscatalog/entities/Product.java
@@ -0,0 +1,126 @@
+package br.com.surb.dscatalog.entities;
+
+import br.com.surb.dscatalog.dto.CategoryDTO;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
+  private static final long serialVersionUID = 694675675061996281L;
+
+  @Id
+  private Long id;
+  private String name;
+  private String description;
+  private Double price;
+  private String imgUrl;
+
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private Instant createdAt;
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private Instant updatedAt;
+
+  
+  Set<Category> categories = new HashSet<>();
+
+  public Product(){}
+
+  public Product(Long id, String name, String description, Double price, String imgUrl, Instant createdAt) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.imgUrl = imgUrl;
+    this.createdAt = createdAt;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public Instant getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Instant createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Instant getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Instant updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Set<Category> getCategories() {
+    return categories;
+  }
+
+  @PrePersist
+  public void preCreate(){
+    createdAt = Instant.now();
+  }
+
+  @PreUpdate
+  public void preUpdate(){
+    updatedAt = Instant.now();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Product product = (Product) o;
+    return id.equals(product.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
