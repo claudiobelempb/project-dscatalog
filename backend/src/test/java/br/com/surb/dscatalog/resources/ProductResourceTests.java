7706e0e3b553f18b4d1610ece124b2570fb5fe80
claudiobelempb <claudio.c.lima@hotmail.com>
2021-09-30 21:04:00 -0300

    02-38 Teste de integração na camada web findAll

--- backend/src/test/java/br/com/surb/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/br/com/surb/dscatalog/resources/ProductResourceTests.java
@@ -3,8 +3,7 @@ package br.com.surb.dscatalog.resources;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -55,6 +54,8 @@ public class ProductResourceTests {
     productDTO = Factory.createProductDTO();
     page = new PageImpl<>(List.of(productDTO));
 
+    when(productService.create(any())).thenReturn(productDTO);
+
     when(productService.index(any())).thenReturn(page);
 
     when(productService.show(existingId)).thenReturn(productDTO);
@@ -68,6 +69,21 @@ public class ProductResourceTests {
     doThrow(DataBaseException.class).when(productService).delete(dependentId);
   }
 
+  /*insert deveria etornar “created” (código 201), bem com um ProductDTO*/
+  @Test
+  public void createShouldReturnProductDTO() throws Exception {
+    String jsonBody = objectMapper.writeValueAsString(productDTO);
+    ResultActions resultActions =
+      mockMvc.perform(post("/products")
+        .content(jsonBody)
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON));
+    resultActions.andExpect(status().isCreated());
+    resultActions.andExpect(jsonPath("$.id").exists());
+    resultActions.andExpect(jsonPath("$.name").exists());
+    resultActions.andExpect(jsonPath("$.description").exists());
+  }
+
   @Test
   public void indexShouldReturnPage() throws Exception{
     ResultActions resultActions =
@@ -76,6 +92,7 @@ public class ProductResourceTests {
     resultActions.andExpect(status().isOk());
   }
 
+  /*findById deveria retornar um ProductDTO quando o id existir*/
   @Test
   public void showShouldReturnProductWhenIdExists() throws Exception{
     ResultActions resultActions =
@@ -87,6 +104,7 @@ public class ProductResourceTests {
     resultActions.andExpect(jsonPath("$.description").exists());
   }
 
+  /*findById deveria lançar ResourceNotFoundException quando o id não existir*/
   @Test
   public void showShouldReturnNotFoundWhenIdDoesNotExists() throws Exception{
     ResultActions resultActions =
@@ -95,8 +113,9 @@ public class ProductResourceTests {
       resultActions.andExpect(status().isNotFound());
   }
 
+  /*update deveria (dica: você vai ter que simular o comportamento do getOne) retornar um ProductDTO quando o id existir*/
   @Test
-  public void updateShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+  public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
     String jsonBody = objectMapper.writeValueAsString(productDTO);
     ResultActions resultActions =
       mockMvc.perform(put("/products/{id}", existingId)
@@ -109,8 +128,9 @@ public class ProductResourceTests {
     resultActions.andExpect(jsonPath("$.description").exists());
   }
 
+  /*update deveria (dica: você vai ter que simular o comportamento do getOne) ançar uma ResourceNotFoundException quando o id não existir*/
   @Test
-  public void updateShouldReturnProductDTOWhenIdNotExists() throws Exception {
+  public void updateShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
     String jsonBody = objectMapper.writeValueAsString(productDTO);
     ResultActions resultActions =
       mockMvc.perform(put("/products/{id}", nonExistingId)
@@ -120,4 +140,21 @@ public class ProductResourceTests {
     resultActions.andExpect(status().isNotFound());
   }
 
+  /*delete deveria retornar “no content” (código 204) quando o id existir*/
+  @Test
+  public void deleteShouldReturnNoContentWhenIdExists() throws Exception {
+    ResultActions resultActions =
+      mockMvc.perform(delete("/products/{id}", existingId)
+        .accept(MediaType.APPLICATION_JSON));
+    resultActions.andExpect(status().isNoContent());
+  }
+
+  /*delete deveria retornar “not found” (código 404) quando o id não existir*/
+  @Test
+  public void deleteShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+    ResultActions resultActions =
+      mockMvc.perform(delete("/products/{id}", nonExistingId)
+        .accept(MediaType.APPLICATION_JSON));
+    resultActions.andExpect(status().isNotFound());
+  }
 }
