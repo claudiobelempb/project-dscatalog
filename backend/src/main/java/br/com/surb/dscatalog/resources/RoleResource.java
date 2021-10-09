6cc64e5a316c073edcf80f3babe2479c8dbceb69
claudiobelempb <claudio.c.lima@hotmail.com>
2021-10-08 22:03:55 -0300

    CRUD de roles complete

--- backend/src/main/java/br/com/surb/dscatalog/resources/RoleResource.java
+++ backend/src/main/java/br/com/surb/dscatalog/resources/RoleResource.java
@@ -1,4 +1,56 @@
 package br.com.surb.dscatalog.resources;
 
+import br.com.surb.dscatalog.dto.RoleDTO;
+import br.com.surb.dscatalog.entities.Role;
+import br.com.surb.dscatalog.services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/roles")
 public class RoleResource {
+
+  @Autowired
+  private RoleService roleService;
+
+  @GetMapping
+  public ResponseEntity<Page<RoleDTO>> index(Pageable pageable){
+    Page<RoleDTO> roles = roleService.index(pageable);
+    return ResponseEntity.ok().body(roles);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<RoleDTO> show(@PathVariable Long id) {
+    RoleDTO roleDTO = roleService.show(id);
+    return ResponseEntity.ok().body(roleDTO);
+  }
+
+  @PostMapping
+  public ResponseEntity<RoleDTO> create(@RequestBody RoleDTO roleDTO){
+    roleDTO = roleService.create(roleDTO);
+    URI uri = ServletUriComponentsBuilder
+      .fromCurrentRequestUri().path("/id")
+      .buildAndExpand(roleDTO.getId())
+      .toUri();
+    return ResponseEntity.ok().body(roleDTO);
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<RoleDTO> update(@PathVariable Long id, @RequestBody RoleDTO roleDTO) {
+    roleDTO = roleService.update(id, roleDTO);
+    return ResponseEntity.ok().body(roleDTO);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    roleService.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+
 }
--- backend/src/main/java/br/com/surb/dscatalog/services/RoleService.java
+++ backend/src/main/java/br/com/surb/dscatalog/services/RoleService.java
@@ -1,4 +1,69 @@
 package br.com.surb.dscatalog.services;
 
+import br.com.surb.dscatalog.dto.RoleDTO;
+import br.com.surb.dscatalog.entities.Role;
+import br.com.surb.dscatalog.repositories.RoleRepository;
+import br.com.surb.dscatalog.services.exceptions.DataBaseException;
+import br.com.surb.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
 public class RoleService {
+
+  @Autowired
+  private RoleRepository roleRepository;
+
+  @Transactional(readOnly = true)
+  public Page<RoleDTO> index(Pageable pageable) {
+    Page<Role> roles = roleRepository.findAll(pageable);
+    return roles.map(role -> new RoleDTO((role)));
+  }
+
+  @Transactional(readOnly = true)
+  public RoleDTO show(Long id) {
+    Optional<Role> optionalRole = roleRepository.findById(id);
+    Role role = optionalRole.orElseThrow(() -> new ResourceNotFoundException("Role not found"));
+    return new RoleDTO(role);
+  }
+
+  @Transactional
+  public RoleDTO create(RoleDTO roleDTO){
+    Role role = new Role();
+    role.setAuthority(roleDTO.getAuthority());
+    role = roleRepository.save(role);
+    return new RoleDTO(role);
+  }
+
+  @Transactional
+  public RoleDTO update(Long id, RoleDTO roleDTO) {
+    try {
+      Role role = roleRepository.getOne(id);
+      role.setAuthority(roleDTO.getAuthority());
+      role = roleRepository.save(role);
+      return new RoleDTO(role);
+    }catch (EntityNotFoundException e){
+      throw new ResourceNotFoundException("Id not found" + id);
+    }
+  }
+
+  @Transactional
+  public void delete(Long id) {
+    try {
+      roleRepository.deleteById(id);
+    }catch (EmptyResultDataAccessException e){
+      throw new ResourceNotFoundException("Id not found " + id);
+    }catch (DataIntegrityViolationException e) {
+      throw new DataBaseException("Integrity violation");
+    }
+  }
+
 }
