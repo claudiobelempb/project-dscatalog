42584f4ec41bdc03dca1b0cc4c513eadeb75228f
claudiobelempb <claudio.c.lima@hotmail.com>
2021-10-18 05:02:54 -0300

    03-27 Configuração especial para o banco H2

--- backend/src/main/java/br/com/surb/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/br/com/surb/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,9 @@
 package br.com.surb.dscatalog.config;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,11 +11,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-  private static final String[] PUBLIC = { "/oauth/token" };
+  @Autowired
+  private Environment environment;
+
+  private static final String[] PUBLIC = {
+    "/oauth/token",
+    "/h2-console/**"
+  };
 
   private static final String[] OPERATOR_OR_ADMIN = {
     "/products/**",
@@ -32,6 +42,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
+
+    // H2
+    if(Arrays.asList(environment.getActiveProfiles()).contains("test")){
+      http.headers().frameOptions().disable();
+    }
+
     http.authorizeRequests()
       .antMatchers(PUBLIC).permitAll()
       .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
